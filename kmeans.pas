315d82152d3396dacc0dc368a008a4c38491c595
Mjic99 <30482039+Mjic99@users.noreply.github.com>
2018-11-17 14:13:07 -0500

    convergence loop

--- kmeans.pas
+++ kmeans.pas
@@ -3,11 +3,10 @@ uses Crt, SysUtils;
 
 var
   X: array [0..149, 0..1] of real;
-  Centroids: array [0..2, 0..1] of real;
-  pointClass: array [0..149] of integer;
-  sums: array [0..2, 0..1] of real;
+  Centroids, sums: array [0..2, 0..1] of real;
+  pointClass, prevClass: array [0..149] of integer;
   counts: array [0..2] of integer;
-  i, j: integer;
+  i, j, iterCount: integer;
 
   Userfile: text;
   readchar: char;
@@ -35,7 +34,24 @@ begin
   end;
 end;
 
+function checkConvergence(): boolean;
+var
+  equal: boolean;
 begin
+  equal:=true;
+  for i:=0 to length(pointClass)-1 do
+      if (pointClass[i]<>prevClass[i]) then
+      begin
+        equal:=false;
+        break;
+      end;
+  if equal then writeln('Terminado!');
+  checkConvergence:=equal;
+end;
+
+begin
+
+  iterCount := 0;
   DecimalSeparator:='.';
 
   system.assign(UserFile,'clean.csv');
@@ -78,31 +94,37 @@ begin
       Centroids[i,0]:=random(round(maxx-minx))+minx;
       Centroids[i,1]:=random(round(maxy-miny))+miny;
   end;
-  //METER PASO 1 Y PASO 2 EN UN WHILE HASTA CONVERGENCIA
-  //Paso 1: Calcular distancias y asignar
-  for i:=0 to length(X)-2 do
-  begin
-      mindist := 100000;
-      for j:=0 to length(Centroids)-1 do
-         if (mindist>dist(X[i,0],Centroids[j,0],X[i,1],Centroids[j,1])) then
-         begin
-           mindist:= dist(X[i,0],Centroids[j,0],X[i,1],Centroids[j,1]);
-           pointClass[i]:= j;
-         end;
-  end;
 
-  //Paso 2: calcular nuevos centroides
-  for i:=0 to length(X)-2 do
-  begin
-      sums[pointClass[i],0] := sums[pointClass[i],0]+X[i,0];
-      sums[pointClass[i],1] := sums[pointClass[i],1]+X[i,1];
-      inc(counts[pointClass[i]]);
-  end;
-  for i:=0 to length(Centroids)-1 do
-  begin
-      Centroids[i,0]:=sums[i,0]/counts[i];
-      Centroids[i,1]:=sums[i,1]/counts[i];
-  end;
+  repeat
+    prevClass:=pointClass;
+    //Paso 1: Calcular distancias y asignar
+    for i:=0 to length(X)-2 do
+    begin
+        mindist := 100000;
+        for j:=0 to length(Centroids)-1 do
+           if (mindist>dist(X[i,0],Centroids[j,0],X[i,1],Centroids[j,1])) then
+           begin
+             mindist:= dist(X[i,0],Centroids[j,0],X[i,1],Centroids[j,1]);
+             pointClass[i]:= j;
+           end;
+    end;
+
+    //Paso 2: calcular nuevos centroides
+    for i:=0 to length(X)-2 do
+    begin
+        sums[pointClass[i],0] := sums[pointClass[i],0]+X[i,0];
+        sums[pointClass[i],1] := sums[pointClass[i],1]+X[i,1];
+        inc(counts[pointClass[i]]);
+    end;
+    for i:=0 to length(Centroids)-1 do
+    begin
+        Centroids[i,0]:=sums[i,0]/counts[i];
+        Centroids[i,1]:=sums[i,1]/counts[i];
+    end;
+
+    inc(iterCount);
+  until checkConvergence();
+  writeln('Ejecutado en ',iterCount,' iteraciones');
   ReadKey;
 end.
 
